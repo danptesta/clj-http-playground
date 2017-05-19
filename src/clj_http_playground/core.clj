38c502e0e60f8152c7070c8eab75125f8f12947f
Daniel Testa <daniel.testa@autodesk.com>
2017-05-19 14:37:51 -0700

    completed web scripting exercise

--- src/clj_http_playground/core.clj
+++ src/clj_http_playground/core.clj
@@ -3,16 +3,67 @@
             [clj-http.cookies :as cookies])
   (:import [org.jsoup Jsoup]))
 
-(let [cookie-store (cookies/cookie-store)
-      login-page (http/get "https://darksky.net/dev/login"
-                           {:cookie-store cookie-store})
-      login-doc (-> login-page
-                    :body
-                    Jsoup/parse)]
-  (:status login-page))
+;;(def email "test@example.com")
+;;(def password "insert-password-here")
 
-(def response (http/get "https://api.darksky.net/forecast/dc6265a401305dd6da2db3db0e014bd6/37.8267,-122.4233" {:as :json}))
+(defn parse-body [page]
+  (-> page
+      :body
+      Jsoup/parse))
 
-(:status response)
-(:server (:headers response))
-(:body response)
+(defn parse-csrf [doc]
+  (-> doc
+      (.select "[name=_csrf]")
+      first
+      (.attr "value")))
+
+(defn login [cookie-store email password]
+  (let [login-page (http/get "https://darksky.net/dev/login"
+                             {:cookie-store cookie-store})
+        login-doc (parse-body login-page)
+        _csrf (parse-csrf login-doc)
+        home-page (http/post "https://darksky.net/dev/login"
+                        {:force-redirects true
+                         :cookie-store cookie-store
+                         :form-params {:_csrf _csrf
+                                       :email email
+                                       :password password}})]
+    home-page))
+
+(defn parse-api-key [doc]
+  (-> doc
+      (.select "[id=api-key]")
+      first
+      (.attr "value")))
+
+(defn fetch-home-doc [cookie-store email password]
+  (let [home-page (login cookie-store email password)
+        home-doc (parse-body home-page)]
+    home-doc))
+
+(defn fetch-api-key [email password]
+  (let [cookie-store (cookies/cookie-store)
+        home-doc (fetch-home-doc cookie-store email password)
+        api-key (parse-api-key home-doc)]
+    api-key))
+
+(defn fetch-reset-key-doc [cookie-store csrf api-key]
+  (let [reset-key-page (http/post "https://darksky.net/dev/account/reset/api-key"
+                                  {:force-redirects true
+                                   :cookie-store cookie-store
+                                   :form-params {:_csrf csrf
+                                                 :api-key api-key}})
+        reset-key-doc (parse-body reset-key-page)]
+    reset-key-doc))
+
+(defn reset-api-key [email password]
+  (let [cookie-store (cookies/cookie-store)
+        home-doc (fetch-home-doc cookie-store  email password)
+        csrf (parse-csrf home-doc)
+        api-key (parse-api-key home-doc)
+        reset-key-doc (fetch-reset-key-doc cookie-store csrf api-key)
+        reset-api-key (parse-api-key reset-key-doc)]
+    reset-api-key))
+
+;;(reset-api-key email password)
+;;(fetch-api-key email password)
